0daf09276bedc76318187ff9bba94a1e85c7f8a4
Oleksiy Nesterov <oleksiy.nesterov+github@gmail.com>
2022-07-22 10:50:11 +0200

    rename.sh

---
+++ rename.sh
@@ -0,0 +1,22 @@
+read -p 'New Component Name (like a "my-component"): ' name
+
+tag=`node -p "\"$name\".toLowerCase().replace(/[^a-z]+/g, '-')"`
+element=`node -p "\"$tag\".replace(/^./, s => s.toUpperCase()).replace(/\-+(.)/g, (_, s) => s.toUpperCase())+'Element'"`
+
+echo
+echo "HTML tag: <$tag></$tag>"
+echo "HTML element: $element"
+echo
+
+read -p "Continue (y/n)? " continue
+if [[ $continue =~ ^[Yy]$ ]] 
+then
+# for f in src/index.ts example/index.html package.json README.md; do
+#   echo $f;
+#   if test -f $f; then
+#     sed -i '' -e "s/my-component/$tag/" $f
+#     sed -i '' -e "s/MyComponentElement/$element/" $f
+#   fi
+# done
+echo
+fi
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 class MyComponentElement extends HTMLElement {
   private shadow: ShadowRoot | null = null;
 
-  constructor() {
-    super();
-  }
-
-  static get observedAttributes() {
-    return ['my-attribute'];
-  }
-
-  emit(eventName: string) {
+  private emit(eventName: string) {
     const event = new CustomEvent(eventName, {
       detail: {
         element: this.shadow
@@ -33,15 +25,23 @@ class MyComponentElement extends HTMLElement {
     }
   }
 
+  constructor() {
+    super();
+  }
+
+  static get observedAttributes() {
+    return ['some-attribute'];
+  }
+
   attributeChangedCallback(name: string, oldValue: string) {
-    if (oldValue && name === 'my-attribute') {
+    if (oldValue && name === 'some-attribute') {
       // todo something... 
     }
   }
 
   connectedCallback() {
     this.shadow = this.attachShadow({mode: 'open'});
-    this.shadow.innerHTML = '<div>My Component</div>';
+    this.shadow.innerHTML = '<div>MyComponentElement Demo</div>';
 
     // todo something... 
 
